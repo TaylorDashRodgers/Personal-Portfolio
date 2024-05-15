8ff0b1d508f8655e3b9ed8f02abbb0eb203de8f3
TaylorDashRodgers <tdrodgers23@gmail.com>
2024-05-15 15:27:14 -0600

    Added another graphics project A2

--- src/data/constants.js
+++ src/data/constants.js
@@ -293,7 +293,20 @@ export const Bio = {
     },
     {
       id: 5,
-      title: "Graphics Create Hero",
+      title: "Graphics Create 2D Hero",
+      date: "Sep 2023",
+      description:
+        "The program creates my 512x512 window where the background is a random hero home from the class. On top of this background we draw our hero avatar made up of individual parts all put together to make one. Our avatar is able to move using WASD and also interacts with the mouse based on where the mouse is and travels to where it is clicked. Along with this if you press left shift and mouse click it will change the backgrounds with out having to go off the sides to wrap around and change the background.",
+      image:
+        "https://github.com/TaylorDashRodgers/a2/assets/74731025/605dded9-5477-4ac8-9bc7-e1c7fe818807",
+      tags: ["C++", "OpenGL"],
+      category: "graphics",
+      github: "https://github.com/TaylorDashRodgers/a2",
+      webapp: "file:///Users/taylorrodgers/CSCI441/www/Deku.html",
+    },
+    {
+      id: 6,
+      title: "Graphics Create 3D Hero",
       date: "Oct 2023",
       description:
         "This program generates a 3D environment with a ground plan across the XZ plane where my hero is drawn to and can be moved around and looked at with an ArcballCam.",
